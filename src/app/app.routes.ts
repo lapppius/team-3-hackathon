c586381c92b8d03b2047ba004166e70e3365ff4b
lapppius <73820845+lapppius@users.noreply.github.com>
2024-05-15 16:06:49 +0300

    added some route paths and edit user component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,6 +14,15 @@ export const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent,
+  },
+  {
+    path: 'profile',
+    children: [
+      {
+        path: 'edit',
+        component: EditUserComponent,
+      },
+    ],
   },
   {
     path: '**',
--- src/app/components/profile-header/profile-header.component.ts
+++ src/app/components/profile-header/profile-header.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Payer } from '../../models/app-models';
 import { DatePipe } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile-header',
   standalone: true,
-  imports: [DatePipe, ButtonModule],
+  imports: [DatePipe, ButtonModule, RouterLink],
   templateUrl: './profile-header.component.html',
   styleUrl: './profile-header.component.scss',
 })
